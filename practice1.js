79f7826a4f34ef21a234f4b07711f11be322ee2b
Larry Adigun <larryadigun.student@careerdevs.com>
2019-01-16 00:34:18 +0000

    Add-on

--- practice1.js
+++ practice1.js
@@ -9,3 +9,33 @@ var server = http.createServer(function(req,res){
 
 server.listen(3000, "127.0.0.1");
 console.log("yo dawgs");
+
+//Video 12
+
+var http = require("http")
+var server = http.createServer(function(req,res){
+    res.writeHead(200, {"Content-Type": "text/plain"});
+    res.end("Hey Ninjas")
+});
+
+server.listen(3000, "127.0.0.1");
+console.log("yo dawgs");
+
+//Video 13
+//Buffer - temporary storage space for a large chunk of data
+//Stream of data that flows over time from one place to another
+
+//video 14
+//writable streams- allow node js to write data to  streams
+//readable stream- read data to the streams
+//Dupleax- misture of both
+var http = require ("http");
+var fs = require ("fs");
+var myReadStream = fs.createReadStream(__dirname + '/readMe.txt', "utf8");
+
+myReadStream.on("data", function(chunk) {
+  console.log("new chunk received")
+  console.log(chunk)
+})
+
+//video 15
