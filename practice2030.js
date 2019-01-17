39a00909ae51f40834697be6a1c36f80d06525fe
Larry Adigun <larryadigun.student@careerdevs.com>
2019-01-17 21:02:10 +0000

    20-30

--- practice2030.js
+++ practice2030.js
@@ -40,3 +40,50 @@ app.get("/profile/:id", function(req,res){
 });
 
 app.listen(3000);
+//video25-Use template engines inject dynamic data and databases!
+// var express = require("express");
+//
+// var app = express();
+//
+// app.set("view engine", "ejs")
+//
+//
+// app.get("/", function(req,res){
+//   res.sendFile(__dirname + "/index.html")
+// });
+//
+// app.get("/contact", function(req,res){
+//   res.sendFile(__dirname + "/contact.html")
+// });
+// //use to output data from a js file, check html
+// app.get("/profile/:name", function(req,res){
+//   var data = {age: 29, job: "ninja"};
+//
+//   res.render("profile", {person: req.params.name, data: data});
+// });
+//
+// app.listen(3000);
+
+//Video 26-Testing engines continued
+var express = require("express");
+
+var app = express();
+
+app.set("view engine", "ejs")
+
+
+app.get("/", function(req,res){
+  res.sendFile(__dirname + "/index.html")
+});
+
+app.get("/contact", function(req,res){
+  res.sendFile(__dirname + "/contact.html")
+});
+//use to output data from a js file, check html
+app.get("/profile/:name", function(req,res){
+  var data = {age: 29, job: "ninja", hobbies:["eating","fighting","walking"]};
+
+  res.render("profile", {person: req.params.name, data: data});
+});
+
+app.listen(3000);
