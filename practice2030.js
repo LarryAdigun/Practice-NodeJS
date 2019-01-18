eaa2683ef1c1f797d037480ee0072b0e02fbeee9
Larry Adigun <larryadigun.student@careerdevs.com>
2019-01-18 15:14:59 +0000

    20-30

--- practice2030.js
+++ practice2030.js
@@ -87,3 +87,27 @@ app.get("/profile/:name", function(req,res){
 });
 
 app.listen(3000);
+
+//video27-Creating Partial Views 
+var express = require("express");
+
+var app = express();
+
+app.set("view engine", "ejs")
+
+
+app.get("/", function(req,res){
+  res.render("index")
+});
+
+app.get("/contact", function(req,res){
+  res.render("contact")
+});
+//use to output data from a js file, check html
+app.get("/profile/:name", function(req,res){
+  var data = {age: 29, job: "ninja", hobbies: ["eating","fighting","walking"]};
+
+  res.render("profile", {person: req.params.name, data: data});
+});
+
+app.listen(3000);
